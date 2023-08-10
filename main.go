819fcdcd7068d36ea45e3ee6eee334a4f55ab48b
Paul <paul21479@gmail.com>
2023-08-10 19:01:07 +1200

    Initial Commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r:=gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "ping",
+		})
+	})
+	
+	r.Run()
+}
